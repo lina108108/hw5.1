e2d8e83830ea199ab982ed9398c18eddf09cbd5a
Liana Veldiaeva <lianali108@gmail.com>
2020-08-03 18:24:07 +0200

    tests add

---
+++ src/test/java/ru/netology/web/DataGenerator.java
@@ -0,0 +1,49 @@
+package ru.netology.web;
+
+import com.github.javafaker.Faker;
+import lombok.Data;
+
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+@Data
+public class DataGenerator {
+    DataGenerator() {
+    }
+
+    public static String getCity() {
+        Random rand = new Random();
+        List<String> list = Arrays.asList("Москва", "Санкт-Петербург", "Казань", "Воронеж", "Волгоград", "Екатеринбург", "Саратов", "Новосибирск", "Владикавказ", "Владивосток", "Ярославль", "Краснодар");
+        int randomIndex = rand.nextInt(list.size());
+        String randomElement = list.get(randomIndex);
+        return randomElement;
+    }
+
+    public static String getRelevantDate(int days) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate today = LocalDate.now();
+        LocalDate date = today.plusDays(days);
+        date.format(formatter);
+        return date.format(formatter);
+    }
+
+    public static String getIrrelevantDate() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate today = LocalDate.now();
+        today.format(formatter);
+        return today.format(formatter);
+    }
+
+
+    public static String getName() {
+        Faker faker = new Faker(new Locale("ru"));
+        return faker.name().lastName() + " " + faker.name().firstName();
+    }
+
+    public static String getPhone() {
+        Faker faker = new Faker(new Locale("ru"));
+        return faker.phoneNumber().phoneNumber();
+    }
+}
---
+++ src/test/java/ru/netology/web/FormTest.java
@@ -0,0 +1,104 @@
+package ru.netology.web;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+import static org.openqa.selenium.By.cssSelector;
+import static ru.netology.web.DataGenerator.*;
+
+
+public class FormTest {
+
+    private String City = getCity();
+    private String Date = getRelevantDate(5);
+    private String OtherDate = getRelevantDate(10);
+    private String InvalidDate = getIrrelevantDate();
+    private String Name = getName();
+    private String Phone = getPhone();
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
+
+    @Test
+    void shouldSubmitRequest() {
+        open("http://localhost:9999");
+        SelenideElement form = $("[action]");
+        form.$(cssSelector("[data-test-id=city] input")).sendKeys(City);
+        form.$(cssSelector("[data-test-id=date] input")).sendKeys(Keys.chord(Keys.CONTROL, "a"));
+        form.$(cssSelector("[data-test-id=date] input")).doubleClick().sendKeys(Keys.DELETE);
+        form.$(cssSelector("[data-test-id=date] input")).sendKeys(Date);
+        form.$(cssSelector("[name=name]")).sendKeys(Name);
+        form.$(cssSelector("[name=phone]")).sendKeys(Phone);
+        form.$(cssSelector("[data-test-id=agreement]")).click();
+        form.$(byText("Запланировать")).click();
+        $(cssSelector(".notification__title")).waitUntil(Condition.visible, 15000);
+    }
+
+    @Test
+    void shouldGiveNewDateWhenSameRequest() {
+        open("http://localhost:9999");
+        SelenideElement form = $("[action]");
+        form.$(cssSelector("[data-test-id=city] input")).sendKeys(City);
+        form.$(cssSelector("[data-test-id=date] input")).sendKeys(Keys.chord(Keys.CONTROL, "a"));
+        form.$(cssSelector("[data-test-id=date] input")).doubleClick().sendKeys(Keys.DELETE);
+        form.$(cssSelector("[data-test-id=date] input")).sendKeys(Date);
+        form.$(cssSelector("[name=name]")).sendKeys(Name);
+        form.$(cssSelector("[name=phone]")).sendKeys(Phone);
+        form.$(cssSelector("[data-test-id=agreement]")).click();
+        form.$(byText("Запланировать")).click();
+        $(byText("Успешно!")).waitUntil(Condition.visible, 15000);
+        open("http://localhost:9999");
+        form.$(cssSelector("[data-test-id=city] input")).sendKeys(City);
+        form.$(cssSelector("[data-test-id=date] input")).sendKeys(Keys.chord(Keys.CONTROL, "a"));
+        form.$(cssSelector("[data-test-id=date] input")).doubleClick().sendKeys(Keys.DELETE);
+        form.$(cssSelector("[data-test-id=date] input")).sendKeys(OtherDate);
+        form.$(cssSelector("[name=name]")).sendKeys(Name);
+        form.$(cssSelector("[name=phone]")).sendKeys(Phone);
+        form.$(cssSelector("[data-test-id=agreement]")).click();
+        form.$(byText("Запланировать")).click();
+        $(cssSelector(".notification_status_error .button")).click();
+        $(withText("Успешно!")).waitUntil(Condition.visible, 15000);
+    }
+
+    @Test
+    void shouldNotGiveNewDateWhenInvalidDate() {
+        open("http://localhost:9999");
+        SelenideElement form = $("[action]");
+        form.$(cssSelector("[data-test-id=city] input")).sendKeys(City);
+        form.$(cssSelector("[data-test-id=date] input")).sendKeys(Keys.chord(Keys.CONTROL, "a"));
+        form.$(cssSelector("[data-test-id=date] input")).doubleClick().sendKeys(Keys.DELETE);
+        form.$(cssSelector("[data-test-id=date] input")).sendKeys(Date);
+        form.$(cssSelector("[name=name]")).sendKeys(Name);
+        form.$(cssSelector("[name=phone]")).sendKeys(getPhone());
+        form.$(cssSelector("[data-test-id=agreement]")).click();
+        form.$(byText("Запланировать")).click();
+        $(byText("Успешно!")).waitUntil(Condition.visible, 15000);
+        open("http://localhost:9999");
+        form.$(cssSelector("[data-test-id=city] input")).sendKeys(City);
+        form.$(cssSelector("[data-test-id=date] input")).sendKeys(Keys.chord(Keys.CONTROL, "a"));
+        form.$(cssSelector("[data-test-id=date] input")).doubleClick().sendKeys(Keys.DELETE);
+        form.$(cssSelector("[data-test-id=date] input")).sendKeys(InvalidDate);
+        form.$(cssSelector("[name=name]")).sendKeys(Name);
+        form.$(cssSelector("[name=phone]")).sendKeys(Phone);
+        form.$(cssSelector("[data-test-id=agreement]")).click();
+        form.$(byText("Запланировать")).click();
+        $(byText("Заказ на выбранную дату невозможен")).waitUntil(Condition.visible, 15000);
+    }
+}
